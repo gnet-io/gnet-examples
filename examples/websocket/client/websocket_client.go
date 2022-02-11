3e613de4178c3d81bb07f6f5aa42f24dfee654f2
ccinn <471113744@qq.com>
2022-02-11 20:58:20 +0800

    Add an example of websocket for gnet v2 (#13)

---
+++ examples/websocket/client/websocket_client.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"github.com/panjf2000/gnet/v2/pkg/logging"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "localhost:9080", "http service address")
+
+func main() {
+	flag.Parse()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/"}
+	logging.Infof("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		logging.Fatalf("dial: %s", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				logging.Infof("read err: %s", err)
+				return
+			}
+			logging.Infof("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-done:
+			return
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				logging.Infof("write err: %s", err)
+				return
+			}
+		case <-interrupt:
+			logging.Infof("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				logging.Infof("write close err: %s", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
+}
---
+++ examples/websocket/server/websocket.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+	"github.com/panjf2000/gnet/v2"
+	"github.com/panjf2000/gnet/v2/pkg/logging"
+	"log"
+	"sync/atomic"
+	"time"
+)
+
+type wsServer struct {
+	*gnet.BuiltinEventEngine
+
+	addr      string
+	multicore bool
+	eng       gnet.Engine
+	connected int64
+}
+
+type wsCodec struct {
+	ws bool
+}
+
+func (wss *wsServer) OnBoot(eng gnet.Engine) gnet.Action {
+	wss.eng = eng
+	logging.Infof("echo server with multi-core=%t is listening on %s", wss.multicore, wss.addr)
+	return gnet.None
+}
+
+func (wss *wsServer) OnOpen(c gnet.Conn) ([]byte, gnet.Action) {
+	c.SetContext(new(wsCodec))
+	atomic.AddInt64(&wss.connected, 1)
+	return nil, gnet.None
+}
+
+func (wss *wsServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
+	if err != nil {
+		logging.Warnf("error occurred on connection=%s, %v\n", c.RemoteAddr().String(), err)
+	}
+	atomic.AddInt64(&wss.connected, -1)
+	logging.Infof("conn[%v] disconnected", c.RemoteAddr().String())
+	return gnet.None
+}
+
+func (wss *wsServer) OnTraffic(c gnet.Conn) gnet.Action {
+	if !c.Context().(*wsCodec).ws {
+		_, err := ws.Upgrade(c)
+		logging.Infof("conn[%v] upgrade websocket protocol", c.RemoteAddr().String())
+		if err != nil {
+			logging.Infof("conn[%v] [err=%v]", c.RemoteAddr().String(), err.Error())
+			return gnet.Close
+		}
+		c.Context().(*wsCodec).ws = true
+	} else {
+		msg, op, err := wsutil.ReadClientData(c)
+
+		if err != nil {
+			if _, ok := err.(wsutil.ClosedError); !ok {
+				logging.Infof("conn[%v] [err=%v]", c.RemoteAddr().String(), err.Error())
+			}
+			return gnet.Close
+		}
+		logging.Infof("conn[%v] receive [op=%v] [msg=%v]", c.RemoteAddr().String(), op, string(msg))
+		// This is the echo server
+		err = wsutil.WriteServerMessage(c, op, msg)
+		if err != nil {
+			logging.Infof("conn[%v] [err=%v]", c.RemoteAddr().String(), err.Error())
+			return gnet.Close
+		}
+	}
+
+	return gnet.None
+}
+
+func (wss *wsServer) OnTick() (delay time.Duration, action gnet.Action) {
+	logging.Infof("[connected-count=%v]", atomic.LoadInt64(&wss.connected))
+	return 3 * time.Second, gnet.None
+}
+
+func main() {
+	var port int
+	var multicore bool
+
+	// Example command: go run main.go --port 8080 --multicore=true
+	flag.IntVar(&port, "port", 9080, "server port")
+	flag.BoolVar(&multicore, "multicore", true, "multicore")
+	flag.Parse()
+
+	wss := &wsServer{addr: fmt.Sprintf("tcp://127.0.0.1:%d", port), multicore: multicore}
+
+	// Start serving!
+	log.Println("server exits:", gnet.Run(wss, wss.addr, gnet.WithMulticore(multicore), gnet.WithReusePort(true), gnet.WithTicker(true)))
+}
